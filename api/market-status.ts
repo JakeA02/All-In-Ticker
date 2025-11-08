fbf5206e59bf4073df96f41f098beec7e62491b5
Jake <ezu7mt@virginia.edu>
2025-11-08 17:13:39 -0500

    mod exports fix

--- api/market-status.ts
+++ api/market-status.ts
@@ -1,6 +1,6 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
-module.exports = async function handler(
+export default async function handler(
   req: VercelRequest,
   res: VercelResponse
 ) {
